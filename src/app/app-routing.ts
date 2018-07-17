a5c3a5fe6c5964f99d9eb4745e0f61bb8dc4c975
unknown <geovana.sembraski@gmail.com>
2018-07-17 17:26:24 -0300

    Add footer.component; Add @angular/material into package.json; Add styles.scss; Add sobre.component; Add import styles.scss into menu.component.scss; Add import styles.scss into footer.component.scss; Add tag footer and div.container; Add imports into app.module; Del styles.css.

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { SobreComponent } from './components/sobre/sobre.component';
 
 const APP_ROUTES: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'sobre', component: SobreComponent }
 ];
 
 export const AppRouting = RouterModule.forRoot(APP_ROUTES);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,24 @@ import { AppRouting } from './app-routing';
 import { HomeComponent } from './components/home/home.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { ProdutosService } from './services/produtos.service';
+import { FooterComponent } from './components/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material';
+import { SobreComponent } from './components/sobre/sobre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    FooterComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
-    AppRouting
+    AppRouting,
+    BrowserAnimationsModule,
+    MatToolbarModule 
   ],
   providers: [ProdutosService],
   bootstrap: [AppComponent]
